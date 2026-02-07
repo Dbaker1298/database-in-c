e8ec354b8a7b8d7297df9cff353b3d9767495db8
David Baker <david.baker@updater.com>
2026-02-06 19:05:00 -0500

    feat: implement database file creation and opening functions

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[]) {
   char *filepath = NULL;
   bool newfile = false;
   int c;
+  int dbfd = -1;
 
   while ((c = getopt(argc, argv, "nf:")) != -1) {
     switch (c) {
@@ -44,6 +45,20 @@ int main(int argc, char *argv[]) {
     print_usage(argv);
 
     return 1;
+  }
+
+  if (newfile) {
+     dbfd = create_db_file(filepath);
+     if (dbfd == STATUS_ERROR) {
+       printf("Unable to create database file\n");
+       return -1;
+     }
+  } else {
+    dbfd = open_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to open database file\n");
+      return -1;
+    }
   }
 
   printf("Newfile: %d\n", newfile);
